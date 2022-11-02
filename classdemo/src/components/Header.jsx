5f93b1d2bf4b13ba5a7b3e4b2cdd261211e20160
brittney-johnson <johnson_brittney1@columbusstate.edu>
2022-11-02 23:10:47 +0000

    added body

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -2,26 +2,38 @@ import React from "react";
 
 class Header extends React.Component{
     //constructor
-
+    constructor(props){
+       super(props)
+       this.state={ 
+        count: 0, 
+        theme: window.matchMedia('prefers-color-scheme: dark').matches ? "dark" : "light"
+      }
+    }
     //lifecycle methods
+    //exp ? true : false
 
 
     //render method
     render(){
         const { linkText, linkUrl } = this.props
+        //this.props.linkText
+        console.log(window.matchMedia('prefers-color-scheme: dark').matches)
+        console.log(this.state)
         return(
-            <header className="App-header">
-            <p>
+            <header className={`App-header ${this.state.theme} ${this.state.count}`}>
+            <p className={"testing-this " + this.state.theme}>
                Edit <code>src/App.js</code> and save to reload. */
             </p>
-            <a
-              className="App-link"
-              href={linkUrl}
-              target="_blank"
-              rel="noopener noreferrer"
+            <a className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer"
             >
                 {linkText}
             </a>
+            <span>
+              You have clicked on the button {this.state.count} times!
+            </span>
+            <button onClick={() => this.setState({ count : this.state.count + 1})}>
+              Increment Count
+            </button>
           </header>
         )
 
